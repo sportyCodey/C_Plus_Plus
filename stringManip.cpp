081ad184ee24c7a13ef13a371785feff51e296e2
Drew <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 15:54:37 -0600

    Create stringManip.cpp

---
+++ stringManip.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+   
+    string input = "";
+    string socialX = "xxx-xx-xxxx";
+    
+    getline(cin, input);
+    
+    int nameIndex = static_cast<unsigned int> (input.find(" "));
+    string name = input.substr(0, nameIndex);
+    
+    /* These aren't used, but it took forever to figure out, so here ya go */
+    int socialSecurity_Index = static_cast<unsigned int> (input.find_first_of(" ", nameIndex + 1));
+    string socialSecurity = input.substr(nameIndex + 1, socialSecurity_Index - nameIndex);
+    
+    int userID_Index = static_cast<unsigned int> (input.find_first_of(" ", socialSecurity_Index + 1));
+    string userID = input.substr(socialSecurity_Index + 1, userID_Index - socialSecurity_Index);
+    
+    int password_Index = static_cast<unsigned int> (input.find_first_of(" ", userID_Index + 1));
+    string password = input.substr(userID_Index + 1, password_Index - userID_Index);
+    int passwordLength = static_cast<unsigned int> (password.length());
+    
+    string passwordX = password.insert(0, passwordLength, 'x');
+    passwordX.erase(passwordLength, passwordLength);
+    
+    
+    cout << name << " ";
+    cout << socialX << " ";
+    cout << userID << " ";
+    cout << passwordX << endl;
+    
+    cout << endl << endl;
+    
+    return 0;
+}
