32c9d95740073dc8ed984fca8b9c77efc7bb57d5
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 12:22:53 -0600

    Create patientType.h

---
+++ patientType.h
@@ -0,0 +1,27 @@
+#ifndef H_patientType
+#define H_patientType
+
+#include <string>
+#include "billType.h"
+#include "doctorType.h"
+#include "personType.h"
+
+class patientType : personType
+{
+public:
+	patientType();
+
+	patientType(std::string newName, int newAge, 
+		std::string newPatientID, double newMedicineCharge,
+		double newDoctorCharge, double newRoomCharge, std::string doctorName,
+		int doctorAge, std::string newSpeciality);
+
+	void printPatient() const;
+
+private:
+	billType bill;
+	doctorType patientDoctor;
+	void defaultInitialize();
+};
+#endif 
+
