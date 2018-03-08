ac369b2e96b9eb7d9b6017d0cfbfdc18b05716e9
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 12:27:10 -0600

    Create Driver.cpp

---
+++ Driver.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "patientType.h"
+
+int main()
+{
+	patientType patient("Drew", 24, "700", 45.90, 34.23, 56.78, "Sam", 56, "Surgeon");
+	patientType patient2;
+
+	patient2.printPatient();
+    return 0;
+}
+
