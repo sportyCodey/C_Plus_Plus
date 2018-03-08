f6c436f38d14a30c5bbd47a6705328effd46d808
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 12:21:47 -0600

    Create doctorType.h

---
+++ doctorType.h
@@ -0,0 +1,25 @@
+#ifndef H_doctorType
+#define H_doctorType
+
+#include <string>
+#include "personType.h"
+
+class doctorType : public personType
+{
+public:
+	doctorType();
+
+	doctorType(std::string newName, int age, std::string newSpeciality);
+
+	std::string getSpeciality() const;
+
+	void setSpeciality(std::string newSpeciality);
+
+	void printDoctor() const;
+
+private:
+	std::string speciality;
+
+};
+#endif
+
