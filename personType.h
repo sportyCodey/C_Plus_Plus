33a59f5a329953a9cc7262d0fb57deacae8f509c
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 12:23:19 -0600

    Create personType.h

---
+++ personType.h
@@ -0,0 +1,27 @@
+#ifndef H_personType
+#define H_personType
+
+#include <string>
+
+class personType
+{
+public:
+	personType();
+
+	personType(std::string newName, int newAge);
+
+	std::string getName() const;
+
+	int getAge() const;
+
+	void setName(std::string newName);
+
+	void setAge(int newAge);
+
+	void printPerson() const;
+
+private:
+	std::string name;
+	int age;
+};
+#endif
