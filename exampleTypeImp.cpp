681240acae790893c3a4ce2c687476da82918cf0
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 12:32:53 -0600

    Create exampleTypeImp.cpp

---
+++ exampleTypeImp.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include <iostream>
+#include "exampleType.h"
+
+exampleType::exampleType()
+{
+	x = 10;
+}
+
+void exampleType::doThis(exampleType type)
+{
+	type.x = 100;
+	std::cout << type.x << std::endl;
+}
+
+void exampleType::setX(int newX)
+{
+	x = newX;
+}
+
+void exampleType::printX() const
+{
+	std::cout << x << std::endl;
+}
