a7e33edf9883e0666fbb4fd94340bf15c9b064e2
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 12:25:37 -0600

    Create patientTypeImp.cpp

---
+++ patientTypeImp.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include "patientType.h"
+
+patientType::patientType()
+	:personType()
+{
+	defaultInitialize();
+}
+
+patientType::patientType(std::string newName, int newAge,
+	std::string newPatientID, double newMedicineCharge,
+	double newDoctorCharge, double newRoomCharge, std::string doctorName,
+	int doctorAge, std::string newSpeciality)
+	:personType(newName, newAge)
+{
+	patientDoctor.setName(doctorName);
+	patientDoctor.setAge(doctorAge);
+	patientDoctor.setSpeciality(newSpeciality);
+	bill.setPatientID(newPatientID);
+	bill.setMedicineCharge(newMedicineCharge);
+	bill.setDoctorCharge(newDoctorCharge);
+	bill.setRoomCharge(newRoomCharge);
+}
+
+void patientType::printPatient() const
+{
+	personType::printPerson();
+	bill.printBill();
+	std::cout << "Doctor info:" << std::endl;
+	patientDoctor.printDoctor();
+}
+
+void patientType::defaultInitialize()
+{
+	patientDoctor.setName("");
+	patientDoctor.setAge(0);
+	patientDoctor.setSpeciality("");
+	bill.setPatientID("");
+	bill.setMedicineCharge(0.0);
+	bill.setDoctorCharge(0.0);
+	bill.setRoomCharge(0.0);
+}
