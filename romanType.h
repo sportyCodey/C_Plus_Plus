8e61b399bed0efc567e093cfa1c89e91d49744c6
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 12:35:16 -0600

    Create romanType.h

---
+++ romanType.h
@@ -0,0 +1,22 @@
+#ifndef H_romanType
+#define H_romanType
+
+#include <string>
+
+class romanType
+{
+public:
+	romanType();
+	romanType(std::string romanNumeral);
+	std::string getRomanNumeral() const;
+	void setRomanNumeral(std::string newRomanNumeral);
+	void convert();
+	void print(bool isRoman) const;
+
+private:
+	std::string romanNumeral = "";
+	int convertedRomanNumeral = 0;
+	int getValueFromSingle(char type);
+	int getValueFromTwo(char type[2]);
+};
+#endif 
